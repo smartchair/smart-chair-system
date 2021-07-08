3ef75347410384bfb10e20ac20988a0081f5b8bd
Lucas Santana <38962310+LucasBSantana@users.noreply.github.com>
2021-07-08 18:20:32 -0300

    Readable data
    Nessa versão já conseguimos consumir dados dos sensores e conectar o esp na interwebs

--- boot.py
+++ boot.py
@@ -1,10 +1,8 @@
-def do_connect():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect('Theobaldo', 'Theo031024')
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+import connect
+connect.do_connect()
+#exec(main)
--- main.py
+++ main.py
@@ -2,49 +2,71 @@
 import dht
 from machine import Pin
 from time import sleep
-import requests
-import json
-  
-# defining constants
-API_ENDPOINT = "https://backend-smart-chair.herokuapp.com/log/info" 
-char_id = "1"
-temp_hum_sensor = dht.DHT11(Pin(5))  
+#import requests
+#import json
+from hcLib import HCSR04
+from ldrLib import LDR
+
+# defining constants and initial values
+API_ENDPOINT = "https://backend-smart-chair.herokuapp.com/log/info"
+chair_id = "1"
+sensor = dht.DHT11(Pin(5))
+presence_sensor = HCSR04(trigger_pin=13, echo_pin=12, echo_timeout_us=1000000)
+ldr_sensor = LDR(34)
 session_id = 0
 count_time = 0
+hc04_presence = False
 
-#getting data from sensors
-
+# getting data from sensors
 while True:
-  try:
+    try:
         sleep(2)
-        temp_hum_sensor.measure()
-        dht_temp = temp_hum_sensor.temperature()
-        dht_hum = temp_hum_sensor.humidity()
-        print('Temperature: %3.1f C' %temp)
-        print('Humidity: %3.1f %%' %hum)
-        
-        #handling sessions
-        if hc04_presence = True:
-                session_id += 1
-                count_time = 0
-        elif count_time < 20:
-                count_time += 1
+
+    # getting temperature and humidity
+        sensor.measure()
+        dht_temp = sensor.temperature()
+        dht_hum = sensor.humidity()
+
+    # getting presence
+        if presence_sensor.distance_cm() < 10:
+            hc04_presence = True
         else:
-                session_id = 0
-        
-        # creating post request
-        payload = { "id": session_id, "temp": dht_temp, "chairId": char_id, "presence": hc04_presence, "noise": 0, "lum": ldr_lum, "hum":dht_hum}
-        
-        # sending post request and saving response as response object
-        r = requests.post(url = API_ENDPOINT, data = json.dumps(payload))
+            hc04_presence = False
 
-        
-        
-        
-        # extracting response text 
-        pastebin_url = r.text
-        print("The pastebin URL is:%s"%pastebin_url)
+    # getting luminosity
+        ldr_lum = ldr_sensor.value()
 
-        #handling error
-        except OSError as e:
-                print('Failed to read sensor.')
+    # handling sessions
+        if hc04_presence == True:
+            session_id += 1
+            count_time = 0
+        elif count_time < 20:
+            count_time += 1
+        else:
+            session_id = 0
+
+        print('Temperature: %3.1f C' % dht_temp)
+        print('Humidity: %3.1f %%' % dht_hum)
+        print('Distance: %3.1f cm' % presence_sensor.distance_cm())
+        if hc04_presence == True:
+            print('Presente')
+        else:
+            print('Not Presente')
+        print('Luiz: %3.1f' % ldr_lum)
+        print(session_id)
+        print(count_time)
+
+    # creating post request
+        payload = { "id": session_id, "temp": dht_temp, "chairId": chair_id, "presence": hc04_presence, "noise": 0, "lum": ldr_lum, "hum": dht_hum}
+    
+        print (payload)
+    # sending post request and saving response as response object
+    # r = requests.post(url = API_ENDPOINT, data = json.dumps(payload))
+
+    # extracting response text
+    # pastebin_url = r.text
+    # print("The pastebin URL is:%s"%pastebin_url)
+
+    # handling error
+    except OSError as e:
+        print('Failed to read sensors.')
