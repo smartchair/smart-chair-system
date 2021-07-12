fb5c64035df6d7655952535ac27741b72e48553c
Lucas Santana <38962310+LucasBSantana@users.noreply.github.com>
2021-07-11 22:00:27 -0300

    final version

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ import urequests
 import ujson
 from hcLib import HCSR04
 from ldrLib import LDR
+import network
 
 # defining constants and initial values
 API_ENDPOINT = "https://backend-smart-chair.herokuapp.com/log/info"
-chair_id = "ESP-03"
+chair_id = "ID dispositivo"
 sensor = dht.DHT11(Pin(5))
 presence_sensor = HCSR04(trigger_pin=27, echo_pin=26, echo_timeout_us=1000000)
 ldr_sensor = LDR(34)
-session_id = 0
-count_time = 0
 hc04_presence = False
+sta_if = network.WLAN(network.STA_IF)
+led = Pin(2, Pin.OUT)
 
 # getting data from sensors
 while True:
     try:
+    #check if internet is connected
+        led.value(not sta_if.isconnected())
+        
+    #resting    
         sleep(10)
 
     # getting temperature and humidity
@@ -36,15 +41,6 @@ while True:
     # getting luminosity
         ldr_lum = ldr_sensor.value()
 
-    # handling sessions
-        if hc04_presence == True:
-            session_id += 1
-            count_time = 0
-        elif count_time < 20:
-            count_time += 1
-        else:
-            session_id = 0
-
         print('Temperature: %3.1f C' % dht_temp)
         print('Humidity: %3.1f %%' % dht_hum)
         print('Distance: %3.1f cm' % presence_sensor.distance_cm())
@@ -53,11 +49,9 @@ while True:
         else:
             print('Not Presente')
         print('Luiz: %3.1f' % ldr_lum)
-        print(session_id)
-        print(count_time)
 
     # creating post request
-        payload = { "id": session_id, "temp": dht_temp, "chairId": chair_id, "presence": hc04_presence, "noise": 0, "lum": ldr_lum, "hum": dht_hum}
+        payload = { "temp": dht_temp, "chairId": chair_id, "presence": hc04_presence, "noise": 0, "lum": ldr_lum, "hum": dht_hum}
         api_data = ujson.dumps(payload)
         
     # sending post request and saving response as response object
